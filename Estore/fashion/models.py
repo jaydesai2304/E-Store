6f4f41c5d2568e05278ffbdde14491544b53b6df
jaydesai2304 <jaydesai2304@gmail.com>
2024-06-17 17:35:55 +0530

    update code

--- Estore/fashion/models.py
+++ Estore/fashion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Register(models.Model):
+    fname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    phone_no = models.IntegerField(default='')
+    password = models.CharField(max_length=100)
+    c_password = models.CharField(max_length=100)
---
+++ Estore/fashion/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Register
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.validators import UniqueValidator
+
+class RegisterSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Register
+        fields = '__all__'
+        extra_kwargs = {
+            'fname': {'required': True},
+        }
+        
+    username = serializers.CharField( required=True, validators = [UniqueValidator(queryset= Register.objects.all())])
+    email = serializers.EmailField( required=True, validators = [UniqueValidator(queryset= Register.objects.all())])
+    password = serializers.CharField(write_only=True,required=True,validators=[validate_password])
+    c_password = serializers.CharField(write_only=True, required=True)
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['c_password']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+    
+    def create(self, validated_data):
+        user = Register.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            fname=validated_data['fname'],
+            phone_no=validated_data['phone_no'],
+            password= validated_data['password'],
+            c_password= validated_data['c_password'],
+        )
+
+        # user.set_password(validated_data['password'])
+        user.save()
+
+        return user
