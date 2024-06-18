30117ea37a66f2b8f4def556441ac07633734153
jaydesai2304 <jaydesai2304@gmail.com>
2024-06-18 09:53:51 +0530

    update code

--- Estore/fashion/admin.py
+++ Estore/fashion/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Register
 
-admin.site.register(Register)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("fname","username","email","phone_no","password",)
+admin.site.register(Register, UserAdmin)
