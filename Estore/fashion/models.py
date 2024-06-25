5dcb9e8fe2fcad1fd0e9d021728fb28541862aa5
jaydesai2304 <jaydesai2304@gmail.com>
2024-06-25 15:51:56 +0530

    Update code

--- Estore/fashion/models.py
+++ Estore/fashion/models.py
@@ -16,4 +16,12 @@ class MenProduct(models.Model):
     image = models.ImageField(upload_to='Menproducts/', default='')
     image1 = models.ImageField(upload_to='Menproducts/', default='')
     image2 = models.ImageField(upload_to='Menproducts/', default='')
+
+class WomenProduct(models.Model):
+    name = models.CharField(max_length=100)
+    discription = models.CharField(max_length=100,default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='Menproducts/', default='')
+    image1 = models.ImageField(upload_to='Menproducts/', default='')
+    image2 = models.ImageField(upload_to='Menproducts/', default='')
     
