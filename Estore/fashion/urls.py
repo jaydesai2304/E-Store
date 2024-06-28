3a7334c4d741ac494574751bb62f2416fc5a4b25
Jay Desai <jaydesai2304@gmail.com>
2024-06-28 11:08:00 +0530

    Update Code

--- Estore/fashion/urls.py
+++ Estore/fashion/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('men_product/', views.men_product, name="men_product"),
     path('women_product/', views.women_product, name="women_product"),
     path('kids_product/', views.kids_product, name="kids_product"),
+    path('fashion_product/', views.fashion_product, name="fashion_product"),
     path('product/<str:category>/<int:id>/', views.product_detail, name='product_detail'),
 
     path('reset_password/', ResetpasswordView.as_view(), name="reset_password"),
