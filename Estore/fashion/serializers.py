8321213ebad2fc0b292506f9bcd1b138230b9835
Jay Desai <jaydesai2304@gmail.com>
2024-07-08 17:15:10 +0530

    add serializer code of cart

--- Estore/fashion/serializers.py
+++ Estore/fashion/serializers.py
@@ -143,3 +143,14 @@ class NewsLetterSerializers(serializers.ModelSerializer):
         def create(self, validated_data):
             user = News_Letter.objects.create(**validated_data)
             return user
+        
+
+class CartSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = CartItem
+        fields = "__all__"
+
+        def create(self, validated_data):
+            user = CartItem.objects.create(**validated_data)
+            return user
